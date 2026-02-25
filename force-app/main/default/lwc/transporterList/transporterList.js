a68f640034170440a9867bb6bd3ee4bd90b23e27
First Last <you@email.com>
2026-02-25 08:59:57 +0100

    LWC étape 1

--- force-app/main/default/lwc/transporterList/transporterList.js
+++ force-app/main/default/lwc/transporterList/transporterList.js
@@ -2,15 +2,27 @@ import { LightningElement, wire } from 'lwc';
 import getTarifications from '@salesforce/apex/TransporterSelector.getTarifications';
 
 export default class TransporterList extends LightningElement {
-
-    tarifs;
+    tarifs = [];
 
     @wire(getTarifications)
     wiredTarifs({ data, error }) {
-        if (data) {
-            this.tarifs = data;
+        if (data && Array.isArray(data)) {
+
+            const cheapestId = data[0]?.Id;
+            const fastestId = data[1]?.Id;
+
+            this.tarifs = data.map(t => ({
+                ...t,
+                isCheapest: t.Id === cheapestId,
+                isFastest: t.Id === fastestId
+            }));
+
         } else if (error) {
             console.error(error);
         }
+    }
+
+    handleSelection(event) {
+        this.selectedTransporterId = event.target.value;
     }
 }
