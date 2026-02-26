4ad4bfe96aea5e71baa389b1d5e5d944df7222e3
First Last <you@email.com>
2026-02-26 01:02:38 +0100

    creating record livraison successfully

--- force-app/main/default/lwc/transporterList/transporterList.js
+++ force-app/main/default/lwc/transporterList/transporterList.js
@@ -1,19 +1,35 @@
 import { LightningElement, api, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getTransporters from '@salesforce/apex/TransporterOrchestrator.getAllTransporters';
+import createLivraison from '@salesforce/apex/OrderService.createLivraison';
 
 
 export default class TransporterList extends LightningElement {
     @api recordId;
     @api pays;
     @api typeClient;
+    selectedTarificationId;
 
 
     tarifs = [];
+    
     get transporterLabel() {
     const paysLabel = this.pays ?? '';
     const typeClientLabel = this.typeClient ?? '';
 
-    return `Sélectionnez un Transporteur `;
+    if (paysLabel && typeClientLabel) {
+        return `Sélectionnez un Transporteur (${paysLabel} : Client ${typeClientLabel} )`;
+    }
+
+    if (paysLabel) {
+        return `Sélectionnez un Transporteur (${paysLabel})`;
+    }
+
+    if (typeClientLabel) {
+        return `Sélectionnez un Transporteur (Client ${typeClientLabel} )`;
+    }
+
+    return 'Sélectionnez un Transporteur';
     }
 
     @wire(getTransporters, { orderId: '$recordId' }) 
@@ -37,7 +53,35 @@ export default class TransporterList extends LightningElement {
 
     
 
+    get isCreateDisabled() {
+        return !this.selectedTarificationId;
+        }
+
     handleSelection(event) {
-        this.selectedTransporterId = event.target.value;
-    }
+        this.selectedTarificationId = event.target.value;
+        }
+        
+    handleCreateLivraison() {
+        createLivraison({ orderId: this.recordId, selectedTarificationId: this.selectedTarificationId })
+            .then(result => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Succès',
+                        message: 'Livraison créée avec succès!',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Erreur',
+                        message: 'Erreur lors de la création de la livraison: ' + (error.body?.message || error.message),
+                        variant: 'error'
+                    })
+                );
+            });
+        }
+
+  
 }
